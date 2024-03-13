648bb29f2a5701ebfd95f1499a47fc72e1973740
pruthvi <pruthvirajbarbole55@gmail.com>
2024-03-13 16:22:15 +0530

    updated commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav
+    style={{
+        backgroundColor : "rgb(25,118,210)",
+        color: 'white',
+        padding: '8px 0px'
+
+    }}
+    >
+        <h1>Bank of react</h1>
+    </nav>
+  )
+}
+
+export default Navbar;
